dac8b4c7067d191c28706f2087a4fccba1a8e6e4
David Stark <dave@davidstark.name>
2022-01-11 12:48:20 +0000

    Use '--rebase' for git pulls

--- app/main.py
+++ app/main.py
@@ -9,24 +9,26 @@ import subprocess
 repobase = "/repos/"
 secret_file = "/etc/github_webhook_secret"
 
-with open(secret_file, 'r') as file:
+with open(secret_file, "r") as file:
     secret = file.read().strip()
 
 app = Flask(__name__)  # Standard Flask app
-webhook = Webhook(app, secret=secret) # Defines '/postreceive' endpoint
+webhook = Webhook(app, secret=secret)  # Defines '/postreceive' endpoint
 
-@webhook.hook()        # Defines a handler for the 'push' event
+
+@webhook.hook()  # Defines a handler for the 'push' event
 def on_push(data):
     print(json.dumps(data))
     if data["ref"] == "refs/heads/master":
         repo = data["repository"]["name"]
         url = data["repository"]["ssh_url"]
         if not os.path.isdir(repobase + repo + "/.git"):
-            subprocess.run(['/usr/bin/git', 'clone', url, repobase + repo])
+            subprocess.run(["/usr/bin/git", "clone", url, repobase + repo])
             print("Cloned " + url + " to " + repobase + repo)
         else:
-            subprocess.run(['/usr/bin/git', 'pull'], cwd=repobase + repo)
+            subprocess.run(["/usr/bin/git", "pull", "--rebase"], cwd=repobase + repo)
             print("Pulled " + repobase + repo)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
